6dafccebec60dbeae8f7e31c3d1febd7915c25b4
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-12-10 17:02:15 -0800

    4.5 Format numeric values as strings

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        var intF = NumberFormat.getIntegerInstance();
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        intF.setGroupingUsed(false);
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        var locale = new Locale("de", "DE");
+        var localeFormatter = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number: " + localeFormatter.format(doubleValue));
+
+        var currencyFormatter = NumberFormat.getCurrencyInstance();
+        System.out.println(currencyFormatter.format(doubleValue));
+
+        var df = new DecimalFormat("$00.00");
+        System.out.println(df.format(1));
     }
 }
