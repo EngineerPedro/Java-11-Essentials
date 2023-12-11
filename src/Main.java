55ff0e066b29de3c3a2dbb6b0becfec5cb8be55e
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-12-11 07:09:41 -0800

    5.5 create overload methods

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,39 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        var sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        var i1 = getInput(sc, "Enter value 1: ");
+        var i2 = getInput(sc, "Enter value 2: ");
 
-        double d1 = getInput(sc, "Enter numeric value 1: ");
+        double result = addValues(i1, i2);
+        System.out.println("The sum is " + result);
 
-        double d2 = getInput(sc, "Enter numeric value 2: ");
+        double resultOfFour = addValues(i1, i2, i1, i2);
+        System.out.println("The sum of 4 is " + resultOfFour);
 
-        double result = d1 / d2;
-
-        System.out.println("The answer is " + result);
+        double resultOfMultiple = addValues(i1, i2, i1, i2, i1, i2);
+        System.out.println("The sum of multiple is " + resultOfMultiple);
     }
 
-    private static double getInput(Scanner sc, String prompt) {
+    private static int getInput(Scanner sc, String prompt) {
         System.out.print(prompt);
-        return sc.nextDouble();
+        return sc.nextInt();
+    }
+
+    private static double addValues(int i1, int i2) {
+        return i1 + i2;
+    }
+
+    private static double addValues(int i1, int i2, int i3, int i4) {
+        return i1 + i2 + i3 + i4;
+    }
+
+    private static double addValues(int... values) {
+        int result = 0;
+        for (var value : values) {
+            result += value;
+        }
+        return result;
     }
 
 }
