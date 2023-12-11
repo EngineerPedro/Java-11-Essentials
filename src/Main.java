834f98e0c844d2da57b44701c89c2ba34df5871b
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-12-10 16:48:58 -0800

    4.4 Convert Primitive values to strings

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        var sb = new StringBuilder("Welcome");
-        sb.append(" to California!")
-                .append("M")
-                .append(", Qty: ")
-                .append(4);
-        var s2 = sb.toString();
-        System.out.println(s2);
+        int result = 10 + 12;
+        String answer = "The answer is " + result;
+        System.out.println(answer);
     }
 }
