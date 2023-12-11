db222603bdf5e1b115d4bf42c579312c93df1ee4
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-12-11 07:28:00 -0800

    6.2 Handle exceptions with try catch

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,12 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        var i1 = getInput(sc, "Enter value 1: ");
-        var i2 = getInput(sc, "Enter value 2: ");
-
-        double result = addValues(i1, i2);
-        System.out.println("The sum is " + result);
-
-        double resultOfFour = addValues(i1, i2, i1, i2);
-        System.out.println("The sum of 4 is " + resultOfFour);
-
-        double resultOfMultiple = addValues(i1, i2, i1, i2, i1, i2);
-        System.out.println("The sum of multiple is " + resultOfMultiple);
-    }
-
-    private static int getInput(Scanner sc, String prompt) {
-        System.out.print(prompt);
-        return sc.nextInt();
-    }
-
-    private static double addValues(int i1, int i2) {
-        return i1 + i2;
-    }
-
-    private static double addValues(int i1, int i2, int i3, int i4) {
-        return i1 + i2 + i3 + i4;
-    }
-
-    private static double addValues(int... values) {
-        int result = 0;
-        for (var value : values) {
-            result += value;
+        String s = null;
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return result;
+        System.out.println("I'm not dead yet!");
     }
-
 }
