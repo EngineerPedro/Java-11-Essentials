77fb0e3fe5b28a27d0a65366f456b5c7fdfef330
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-12-10 18:42:16 -0800

    4.11 Simple Calculator

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
 
-        System.out.print("Enter a value: ");
-        var input = scanner.nextLine();
-        System.out.println(input);
+        var sc = new Scanner(System.in);
 
-        System.out.print("Enter number 1: ");
-        var number1 = scanner.nextInt();
-        System.out.print("Enter number 2: ");
-        var number2 = scanner.nextInt();
+        System.out.print("Enter a numeric value: ");
+        var d1 = sc.nextDouble();
 
-        var sum = number1 + number2;
-        System.out.println("The sum is " + sum);
+        System.out.print("Enter a numeric value: ");
+        var d2 = sc.nextDouble();
+
+        double result = d1 / d2;
+
+        System.out.println("The answer is " + result);
     }
 }
