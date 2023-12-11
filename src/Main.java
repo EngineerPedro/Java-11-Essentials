f0d3b474697ffd4603e2dab90b59d48abe274d58
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-12-10 17:07:57 -0800

    4.8 Parse string values

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = "Hello!";
-        var s2 = "Hello!";
+        var s1 = "Welcome to California!";
+        System.out.println("length of string: " + s1.length());
 
-        if (s1 == s2) {
-            System.out.println("they match!");
-        } else {
-            System.out.println("they don't match!");
-        }
+        int position = s1.indexOf("California");
+        System.out.println("The position is " + position);
 
-        String s3 = new String("Hello!");
-        String s4 = new String("Hello!");
-        if (s3 == s4) {
-            System.out.println("they match!");
-        } else {
-            System.out.println("they don't match!");
-        }
+        var sub = s1.substring(11);
+        System.out.println(sub);
 
-        if (s3.equals(s4)) {
-            System.out.println("they match!");
-        } else {
-            System.out.println("they don't match!");
-        }
-
-        var s5 = "HELLO!";
-        if (s3.equalsIgnoreCase(s5)) {
-            System.out.println("they match!");
-        } else {
-            System.out.println("they don't match!");
-        }
+        String s2 = "Welcome!      ";
+        var len = s2.length();
+        System.out.println(len);
+        var trimmed = s2.trim();
+        System.out.println(trimmed.length());
     }
 }
