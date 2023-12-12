9c67a045c9772d67755cd3152e7b3525bcc3bb53
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-12-12 10:21:45 -0800

    7.1 Declare and use custom classes

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,52 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.time.LocalDateTime;
 
 public class Main {
+
     public static void main(String[] args) {
-        String s = "";
+
+        var sc = new Scanner(System.in);
+
+        double d1, d2;
         try {
-            var sub = s.substring(1);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-            System.out.println("Null pointer: " + e.getMessage());
-        } catch (StringIndexOutOfBoundsException e) {
-            e.printStackTrace();
-            System.out.println("Out of bounds: " + e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("I don't know what happened: " + e.getMessage());
+            System.out.print("Enter a numeric value: ");
+            d1 = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.print("Enter a numeric value: ");
+            d2 = sc.nextDouble();
+            sc.nextLine();
+        } catch (InputMismatchException e) {
+            System.out.println("Couldn't format as a number");
+            return;
         }
-        System.out.println("I'm not dead yet!");
+
+        System.out.print("Select an operation (+ - * /): ");
+        var operation = sc.nextLine();
+
+        double result;
+        switch (operation) {
+            case "+":
+                result = CalcHelper.addValues(d1, d2);
+                break;
+            case "-":
+                result = CalcHelper.subtractValues(d1, d2);
+                break;
+            case "*":
+                result = CalcHelper.multiplyValues(d1, d2);
+                break;
+            case "/":
+                result = CalcHelper.divideValues(d1, d2);
+                break;
+            default:
+                System.out.println("You didn't choose a valid operation");
+                return;
+        }
+        System.out.println("The answer is " + result);
     }
 }
